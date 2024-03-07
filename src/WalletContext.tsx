4b24b91b737f65afec96163a0e452847c7b217dd
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-03-07 20:56:12 +0530

    added in react

--- src/WalletContext.tsx
+++ src/WalletContext.tsx
@@ -21,21 +21,43 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http()
-  },
-  connectors: [
-    walletConnect({ projectId, metadata, showQrModal: false }),
-    injected({ shimDisconnect: true }),
-    coinbaseWallet({
-      appName: metadata.name,
-      appLogoUrl: metadata.icons[0]
-    })
-  ]
-})
+// const config = createConfig({
+//   chains: [mainnet, sepolia],
+//   transports: {
+//     [mainnet.id]: http(),
+//     [sepolia.id]: http()
+//   },
+//   connectors: [
+//     walletConnect({ projectId, metadata, showQrModal: false }),
+//     injected({ shimDisconnect: true }),
+//     coinbaseWallet({
+//       appName: metadata.name,
+//       appLogoUrl: metadata.icons[0]
+//     })
+//   ]
+// })
+
+// function createConfig(configObject: ConfigType) {
+//   // Function body
+// }
+
+  const config = createConfig({
+    chains: [mainnet, sepolia],
+    transports: {
+      [mainnet.id]: http(),
+      [sepolia.id]: http()
+    },
+    connectors: [
+      walletConnect({ projectId, metadata, showQrModal: false }),
+      injected({ shimDisconnect: true }),
+      coinbaseWallet({
+        appName: metadata.name,
+        appLogoUrl: metadata.icons[0]
+      })
+    ]
+  });
+
+
 
 // 3. Create modal
 createWeb3Modal({
@@ -44,10 +66,13 @@ createWeb3Modal({
   enableAnalytics: true // Optional - defaults to your Cloud configuration
 })
 
-export function ContextProvider({ children }) {
+// function ContextProvider({ children }) {
+  function ContextProvider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
-  )
+  );
 }
+
+export default ContextProvider
